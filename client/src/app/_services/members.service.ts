0c4ba70aac0e44066277c2315e311606ab38c8a4
RedGhoul <avaneesab5@gmail.com>
2021-05-04 09:25:03 -0400

    Added models and services

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Member} from '../_models/member';
+import {Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {
+  }
+
+  getMembers(): Observable<Member[]>{
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string): Observable<Member>{
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
